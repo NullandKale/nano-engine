80831abd3131fbe062b79f28da04baa9b8570d3a
Alec Zinsli <zinsli.alec@gmail.com>
2020-11-25 18:04:38 -0800

    initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullEngine.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
